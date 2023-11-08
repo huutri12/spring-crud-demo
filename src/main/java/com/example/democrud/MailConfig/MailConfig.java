9ff477e1481df926864d595254a37b6b5d2ab323
David <nevemore6264@gmail.com>
2023-11-08 19:31:52 +0700

    adđ

--- src/main/java/com/example/democrud/MailConfig/MailConfig.java
+++ src/main/java/com/example/democrud/MailConfig/MailConfig.java
@@ -3,5 +3,6 @@ package com.example.democrud.MailConfig;
 public class MailConfig {
     public static void main(String[] args) {
         System.out.printf("hello");
+        System.out.println("xxx");
     }
 }
